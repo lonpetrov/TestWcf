b7b067ca62bb21c77f409bea5ac5bbc8038a570c
ilya petrov <pil@etz-vektor.ru>
2021-03-10 11:27:08 +0400

    Add project files.

---
+++ TestWcf/Cheque.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWcf
+{
+    public class Cheque : ICheque
+    {
+        public Guid Id { get; set; }
+
+        public string Number { get; set; }
+
+        public decimal Summ { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public string[] Articles { get; set; }
+    }
+}
---
+++ TestWcf/IDBRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWcf
+{
+    interface IDBRepository
+    {
+        void SaveCheck(Cheque cheque);
+
+        IEnumerable<Cheque> GetLastChecks(int count);
+    }
+}
